9001ea2dabf33d385bb1766817e6c9ba01bca6c0
José Caldas <jrcaldas@yahoo.com.br>
2024-05-02 09:26:38 -0300

    add Spectral font

---
+++ src/functions/fonts.ts
@@ -0,0 +1,9 @@
+import { Spectral } from 'next/font/google'
+
+export const type_second = Spectral({
+  weight: ['700'],
+  style: ['normal'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--type-second-spectral',
+})
